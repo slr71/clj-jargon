00d2a1bf92f5687bbfd9beff7010bf14df2e3b21
Sarah Roberts <sonoran.sarah@gmail.com>
2026-06-10 16:35:24 -0700

    CORE-2160: refactor clj-jargon.paging/read-at-position
    This function was creating two instances of IRODSRandomAccessFile for the same
    file. One instance was used to obtain the length of the file in bytes. The other
    was used to read the file contents. This worked in previous versions of iRODS,
    but it doesn't appear to work any longer. The version of Jargon doesn't appear
    to have been a problem because this was working with this version of Jargon before
    we upgraded to iRODS 4.3.1.
    
    The file length was used to adjust the size of the read buffer so that we never
    tried to read past the end of the file. The easiest solution is to read past the
    end of the File and let Jargon and iRODS detect the end of the file instead. This
    simplifies the code a little bit as well, because the only edge case we have to
    handle is when we read fewer bytes than we have capacity for in the buffer.
    
    Converting a partial read from a file directly into a string may also cause some
    problems with character encoding. If the encoding scheme has multi-byte characters
    then nothing prevents us from reading a partial character either at the beginning
    or at the end of a buffer. This change does not fix this problem.

--- src/clj_jargon/paging.clj
+++ src/clj_jargon/paging.clj
@@ -1,8 +1,9 @@
 (ns clj-jargon.paging
-  (:import [org.irods.jargon.core.pub.io FileIOOperations
-                                         FileIOOperations$SeekWhenceType
-                                         IRODSRandomAccessFile
-                                         IRODSFileFactory]))
+  (:import
+   [org.irods.jargon.core.pub.io
+    FileIOOperations$SeekWhenceType
+    IRODSRandomAccessFile
+    IRODSFileFactory]))
 
 (def SEEK-CURRENT (FileIOOperations$SeekWhenceType/SEEK_CURRENT))
 (def SEEK-START (FileIOOperations$SeekWhenceType/SEEK_START))
@@ -12,29 +13,18 @@
   [{^IRODSFileFactory file-factory :fileFactory} ^String filepath]
   (.instanceIRODSRandomAccessFile file-factory filepath))
 
-(defn file-length-bytes
-  [cm filepath]
-  (.length (random-access-file cm filepath)))
-
 (defn read-at-position
   ([cm filepath position num-bytes]
    (read-at-position cm filepath position num-bytes true))
   ([cm filepath position num-bytes stringify?]
    (let [access-file (random-access-file cm filepath)
-         file-size   (file-length-bytes cm filepath)
-         array-size  (if (< file-size num-bytes) file-size num-bytes)
-         buffer      (byte-array array-size)]
-     (cond
-       (zero? file-size) ""
-       (zero? num-bytes) ""
-       (neg? num-bytes) ""
-       :else
-       (let [_   (.seek access-file position SEEK-CURRENT)
-             len (.read access-file buffer)
-             _   (.close access-file)]
-         (if stringify?
-           (String. buffer 0 len)
-           buffer))))))
+         buffer      (byte-array num-bytes)]
+     (let [_   (.seek access-file position SEEK-CURRENT)
+           len (.read access-file buffer)
+           _   (.close access-file)]
+       (if stringify?
+         (String. buffer 0 (max 0 len))
+         (byte-array (take (max 0 len) buffer )))))))
 
 (defn overwrite-at-position
   [cm filepath position update]
