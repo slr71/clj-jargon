84428776618a118b9796aeba0f0e7939f1d33ceb
Sarah Roberts <sarahr@cyverse.org>
2026-08-17 16:40:22 -0700

    Version 3.1.6

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.cyverse/clj-jargon "3.1.6-SNAPSHOT"
+(defproject org.cyverse/clj-jargon "3.1.6"
   :description "Clojure API on top of iRODS's jargon-core."
   :url "https://github.com/cyverse-de/clj-jargon"
   :license {:name "BSD"
